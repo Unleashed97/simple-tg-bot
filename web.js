bd5ebd3c2bb1498420a4d79b63ca47d4fcae0d27
Nichkov Roman <nichkovroman@gmail.com>
2021-08-06 17:04:29 +0300

    heroku request fix

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
-const app = express()
-
-app.listen(process.env.PORT)
-
+const app = express();
+ 
+app.listen(process.env.PORT);
+ 
 module.exports = bot =>{
-    app.post('/' + bot.token, (req, res) =>{
-        bot.processUpdate(req.body)
-        res.sendStatus(200)
-    })
+    app.post('/' + bot.token, (req, res) => {
+      bot.processUpdate(req.body);
+      res.sendStatus(200);
+    });
 }
