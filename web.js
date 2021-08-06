a5421205e54e0eb974d703bc0a716c36f9f9f160
Nichkov Roman <nichkovroman@gmail.com>
2021-08-06 19:00:41 +0300

    request fix

--- web.js
+++ web.js
@@ -2,11 +2,24 @@ const express = require('express')
 
 const app = express();
 
-app.listen(process.env.PORT);
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-module.exports = bot =>{
-    app.post('/' + bot.token, (req, res) => {
-        bot.processUpdate(req.body);
-        res.sendStatus(200);
-    });
-}
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+var server = app.listen(process.env.PORT, "0.0.0.0", () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
+});
+
+module.exports = (bot) => {
+  app.post('/' + bot.token, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+};
