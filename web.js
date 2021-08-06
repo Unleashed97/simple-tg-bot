f6faff95ba45945cc2e1de48828f00ee5198fa1d
Nichkov Roman <nichkovroman@gmail.com>
2021-08-06 16:32:08 +0300

    dotenv added

---
+++ web.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(process.env.PORT)
+
+module.exports = bot =>{
+    app.post('/' + bot.token, (req, res) =>{
+        bot.processUpdate(req.body)
+        res.sendStatus(200)
+    })
+}
